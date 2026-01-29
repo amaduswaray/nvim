63bab0ca12a06a5cb3ce40ea720fc3fed439bf14
Amadu Swaray <amaduswaray06@gmail.com>
2026-01-29 20:30:21 +0100

    On attach inline

--- lua/LSP/lspcponfig.lua
+++ lua/LSP/lspcponfig.lua
@@ -6,16 +6,12 @@ return {
 	},
 	config = function()
 		vim.lsp.config("lua_ls", {
-			capabilities = capabilities,
-			on_attach = on_attach,
 			settings = { -- custom settings for lua
 				Lua = {
-					-- make the language server recognize "vim" global
 					diagnostics = {
 						globals = { "vim" },
 					},
 					workspace = {
-						-- make language server aware of runtime files
 						library = {
 							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
 							[vim.fn.stdpath("config") .. "/lua"] = true,
