87fc8cbc1f12438774ec423f79ca4c2d5693a65e
Amadu Swaray <amaduswaray06@gmail.com>
2026-01-29 18:35:48 +0100

    move with shift k n j

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -194,6 +194,8 @@ vim.keymap.set("n", "bd", "<cmd>bdelete<cr>", { desc = "Buffer delete" })
 
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
 
 -- Activating lazy -- remove for 12
