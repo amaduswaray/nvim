0cca32e18e33ea23743146c6411927f818a71b47
Amadu Swaray <amaduswaray06@gmail.com>
2026-01-29 18:37:40 +0100

    inplace J

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -196,6 +196,7 @@ vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
 
 -- Activating lazy -- remove for 12
