8cb74b3401bcca789db9293528065b4eabe2d0cd
Amadu Swaray <amaduswaray06@gmail.com>
2026-01-31 12:45:10 +0100

    lsp descriptions added

--- lua/LSP/lspcponfig.lua
+++ lua/LSP/lspcponfig.lua
@@ -9,16 +9,37 @@ return {
 			callback = function(ev)
 				local opts = { buffer = ev.buf }
 				-- TODO: Set snacks picker for gd, gR, gi
+				opts.desc = "Show LSP definitions"
 				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+
+				opts.desc = "Go to declaration"
 				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+
+				opts.desc = "Show LSP references"
 				vim.keymap.set("n", "gR", vim.lsp.buf.references, opts)
+
+				opts.desc = "Hover"
 				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+				opts.desc = "Show LSP implementations"
 				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+
+				opts.desc = "Rename"
 				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
+				opts.desc = "Restart LSP"
 				vim.keymap.set("n", "<leader>rs", "<CMD>LspRestart<CR>", opts)
+
+				opts.desc = "Go to previous diagnostic"
 				vim.keymap.set("n", "[d", vim.diagnostic.get_prev, opts)
+
+				opts.desc = "Go to next diagnostic"
 				vim.keymap.set("n", "]d", vim.diagnostic.get_next, opts)
+
+				opts.desc = "Show line diagnostics"
 				vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
+
+				opts.desc = "See available code actions"
 				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 
 				vim.keymap.set("n", "<leader>li", function()
